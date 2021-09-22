a388cd278fcf5319a727eba4a5a31445a9519add
TankeuIvan <tankeu.ivan-junior1@etu.univ-lorraine.fr>
2021-09-22 10:48:57 +0200

    Updates...; Déclaration de tous les boutons et écriture standard des méthodes d'opération

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,22 +29,24 @@ namespace Calculatrice
         //string[] charactere = new string[100];
         private int i = 0;
 
-        private void buttonPlus_Click(object sender, RoutedEventArgs e)
+        private void button0_Click(object sender, RoutedEventArgs e)
         {
-            Button bPlus = new Button();
-            bPlus.Content = buttonPlus.Content;
-            OperationEnCours((string)bPlus.Content);
-            
+
+        }
+        private void buttonM1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
 
-        private void button3_Click(object sender, RoutedEventArgs e)
+        private void buttonM2_Click(object sender, RoutedEventArgs e)
         {
-            Button b3 = new Button();
-            b3.Content = button3.Content;
-            affichage.Text = (string)b3.Content;
-            OperationEnCours((string)b3.Content);
+
         }
 
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
         private void button2_Click(object sender, RoutedEventArgs e)
         {
             Button b2 = new Button();
@@ -52,7 +54,60 @@ namespace Calculatrice
             affichage.Text = (string)b2.Content;
             OperationEnCours((string)b2.Content);
         }
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            Button b3 = new Button();
+            b3.Content = button3.Content;
+            affichage.Text = (string)b3.Content;
+            OperationEnCours((string)b3.Content);
+        }
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            Button b4 = new Button();
+            b4.Content = button4.Content;
+            affichage.Text = (string)b4.Content;
+            OperationEnCours((string)b4.Content);
+        }
 
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            Button b5 = new Button();
+            b5.Content = button5.Content;
+            affichage.Text = (string)b5.Content;
+            OperationEnCours((string)b5.Content);
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            Button b6 = new Button();
+            b6.Content = button6.Content;
+            affichage.Text = (string)b6.Content;
+            OperationEnCours((string)b6.Content);
+        }
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            Button b7 = new Button();
+            b7.Content = button7.Content;
+            affichage.Text = (string)b7.Content;
+            OperationEnCours((string)b7.Content);
+        }
+
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+            Button b8 = new Button();
+            b8.Content = button3.Content;
+            affichage.Text = (string)b8.Content;
+            OperationEnCours((string)b8.Content);
+        }
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+            Button b9 = new Button();
+            b9.Content = button9.Content;
+            affichage.Text = (string)b9.Content;
+            OperationEnCours((string)b9.Content);
+        }
+        
         private void OperationEnCours(string s)
         {
             if (i == 0)
@@ -64,7 +119,7 @@ namespace Calculatrice
 
         private void buttonEgal_Click(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show(expression);
+            MessageBox.Show(expression);
             string[] charactere = expression.Split(';');
 
             double resultat = 0;
@@ -75,7 +130,8 @@ namespace Calculatrice
                 {
                     case "+":
                         resultat = Convert.ToDouble(charactere[i - 2]) + Convert.ToDouble(charactere[i]);
-                        charactere[i] = Convert.ToString(resultat);
+                        affichage.Text = Convert.ToString(resultat);
+                       
                         break;
 
                     case "-":
@@ -83,6 +139,15 @@ namespace Calculatrice
                         charactere[i] = Convert.ToString(resultat);
                         break;
 
+                    case "/":
+                        resultat = Convert.ToDouble(charactere[i - 2]) / Convert.ToDouble(charactere[i]);
+                        charactere[i] = Convert.ToString(resultat);
+                        break;
+
+                    case "x":
+                        resultat = Convert.ToDouble(charactere[i - 2]) * Convert.ToDouble(charactere[i]);
+                        charactere[i] = Convert.ToString(resultat);
+                        break;
                 }
             }
             expression = "";
@@ -90,82 +155,43 @@ namespace Calculatrice
             affichage.Text = Convert.ToString(resultat);
             //MessageBox.Show(Convert.ToString(resultat));
             
-
-            
-            
-            
         }
 
-        private void buttonM1_Click(object sender, RoutedEventArgs e)
+
+        private void buttonPlus_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void buttonM2_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void buttonC_Click(object sender, RoutedEventArgs e)
-        {
+            Button bPlus = new Button();
+            bPlus.Content = buttonPlus.Content;
+            OperationEnCours((string)bPlus.Content);
 
         }
 
         private void buttonDiv_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void button7_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void button8_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void button9_Click(object sender, RoutedEventArgs e)
-        {
-
+            Button bDiv = new Button();
+            bDiv.Content = buttonDiv.Content;
+            OperationEnCours((string)bDiv.Content);
         }
 
         private void buttonMul_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void button4_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void button5_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void button6_Click(object sender, RoutedEventArgs e)
-        {
-
+            Button bMul = new Button();
+            bMul.Content = buttonMul.Content;
+            OperationEnCours((string)bMul.Content);
         }
 
         private void buttonMoins_Click(object sender, RoutedEventArgs e)
         {
-
+            Button bMoins = new Button();
+            bMoins.Content = buttonMoins.Content;
+            OperationEnCours((string)bMoins.Content);
         }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void buttonC_Click(object sender, RoutedEventArgs e)
         {
-
+            /*Button bC = new Button();
+            bC.Content = buttonC.Content;
+            OperationEnCours((string)bPlus.Content);*/
         }
-
-        private void button0_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void buttonVirg_Click(object sender, RoutedEventArgs e)
         {
 
